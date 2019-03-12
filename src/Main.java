38637c2174b6aed011e8622aab563dc342564413
vlad <udovenko555@mail.ru>
2019-03-12 19:44:54 +0600

    Создал несколько классов

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         try{
-            Path path = Paths.get("cities.txt");//Путь к списку городов
+            Path path = Paths.get("E:\\Политех\\ООП\\ksrka\\src\\cities.txt");//Путь к списку городов
             List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);//List, созданный из списка городов
         }catch (Exception e){
             System.out.println(e.toString());
