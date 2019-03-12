be273c9101df16ee1e0248c3a96f3651e40803ec
vlad <udovenko555@mail.ru>
2019-03-12 19:42:25 +0600

    Создал несколько классов

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            Path path = Paths.get("cities.txt");//Путь к списку городов
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);//List, созданный из списка городов
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+}
